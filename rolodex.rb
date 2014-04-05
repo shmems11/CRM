75e63f5c0e0065e017e483939b242d9530c5d9c4
shmems11 <shmems11@gmail.com>
2014-04-05 01:06:54 -0400

    rolodex done

--- rolodex.rb
+++ rolodex.rb
@@ -1,10 +1,17 @@
 class Rolodex
-	attr_reader 
+	attr_reader :contacts
 	def initialize
+		@contacts = []
+		@id = 0
 	end
 
-	def add_contact
+	def contacts
+		@contacts
 	end
 
-
+	def add_contact(contact)
+		contact.id = @id
+		@contacts << contact
+		@id += 1
+	end
 end
